56bc2962fe897475f89ee5411aa3743e2c13f24f
Ge Tadeo <mr.genesis.tadeo@gmail.com>
2019-03-27 15:50:05 +0800

    Add diagonal differences

---
+++ diagonalDifferences.js
@@ -0,0 +1,18 @@
+const diagonalDifferences = (arr) => {
+  let leftToRightDiagonal = 0
+  let rightToLeftDiagonal = 0
+  
+  for (let i = 0; i < arr.length; i++) {
+    leftToRightDiagonal += arr[i][i];
+    rightToLeftDiagonal += arr[arr.length - 1 - i][i]
+  }
+    return Math.abs(leftToRightDiagonal - rightToLeftDiagonal)
+}
+
+let matrix = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12]
+]
+
+console.log(diagonalDifferences(matrix))
